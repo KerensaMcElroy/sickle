c19b4e6367ac247c4d364bc5f40577591b856472
Jocelyn <jocelyn@sietsma.com>
2013-10-03 13:41:38 +1000

    Works for valid entries, no sanity testing. Test code at end

--- input_fns.py
+++ input_fns.py
@@ -9,19 +9,19 @@ def get_window_lth(read_lth):
     If window length not valid, default value of 0.1 * read length is returned'''
     prompt_str = "Enter a window length (default is read_length/10): "
     lth = int(raw_input(prompt_str))
-    if not (lth > 0 and lth <= read_lth)
+    if not (lth > 0 and lth <= read_lth):
         lth = read_lth/10
     return lth
 
 def ask_qual_thresh():
     prompt_str = "Enter a quality threshold (1-40): "
-    try:
+    thresh = 0
+    while not (thresh >=1 and thresh <=40):
         thresh = float(raw_input(prompt_str))
-    except:
-        print 'threshold must be a number'
-        raise
-    assert (thresh >=1 and thresh <=40)
+    return thresh
 
-out_str = ask_window_lth()
-print "You entered: ", out_str
+out_var = get_window_lth(120)
+print "You entered: ", out_var
 
+out_var = ask_qual_thresh()
+print "You entered: ", out_var
